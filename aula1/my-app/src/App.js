1a241b238e44ac6d233bc595375884cb46e4f5c6
unknown <brnoamrr@gmail.com>
2021-10-14 00:08:26 -0300

    aula1react

---
+++ aula1/my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aprendendo React</h1>
+      <img src="https://s2.glbimg.com/AFW86yTLGxM7fBVSmzLA1R_addw=/31x0:947x671/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/R/4/eFVj74TU6DffkgtAEDTg/14500529-1766823346903113-7499433856902780915-o.jpg" />
+      <pre>Por influência da mitologia grega, tanto que Atena, deusa da guerra e da sabedoria, tinha uma coruja como mascote. Os gregos consideravam a noite como o momento do pensamento filosófico e da revelação intelectual e a coruja, por ser uma ave noturna, acabou representando essa busca pelo saber.</pre>
+    </div>
+  );
+}
+
+export default App;
